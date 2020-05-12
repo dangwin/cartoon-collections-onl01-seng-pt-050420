1f23e70c02cc76af97ed8ee14d6accf88a90bf35
Dang Nguyen <danghwin@gmail.com>
2020-05-12 23:11:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,12 +27,10 @@ def long_planeteer_calls(calls)
   end
     
   
+
+def find_the_cheese(cheeses)
   
-
-
-
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
